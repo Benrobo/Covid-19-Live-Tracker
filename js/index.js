0cc511bf483881c48ea902f5fea4cc9fd140fb88
benrobo <alumonabenaiah71@gmail.com>
2021-05-18 12:18:56 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+const COUNTRY_API = "https://restcountries.eu/rest/v2/";
+const CASE_API = "https://api.covid19api.com/summary";
+const CASE_API_Country = "https://disease.sh/v3/covid-19/countries";
 
 // get Covid19 cases
 async function getcases(){
-    let CASE_API = "https://disease.sh/v3/covid-19/historical/all?lastdays=90";
     let res = await fetch(CASE_API);
     let data = await res.json();
-
     return data;
 }
 
@@ -13,14 +14,15 @@ async function getdata(){
     let oup = [];
     oup.push(response)
     let newdata = oup.map((data)=>{
-        // console.log(data);
-        let date = Object.keys(data.cases);
-        let recovered = Object.values(data.recovered);
-        let death = Object.values(data.deaths);
-        let total = Object.values(data.cases);
-        let fmatDeath = numeral(death[death.length-1]).format('0,0');
-        let fmatCase = numeral(total[total.length-1]).format('0,0');
-        let fmatRecovered = numeral(recovered[recovered.length-1]).format('0,0');
+        console.log(data);
+        let {TotalConfirmed , TotalRecovered, TotalDeaths} = data.Global
+        console.log(TotalConfirmed);
+        let recovered = TotalRecovered;
+        let death = TotalDeaths;
+        let total = TotalConfirmed;
+        let fmatDeath = numeral(death).format('0,0');
+        let fmatCase = numeral(total).format('0,0');
+        let fmatRecovered = numeral(recovered).format('0,0');
 
         // let objTest = {fmatCase, fmatDeath, fmatRecovered}
         showCounts(fmatCase, fmatDeath, fmatRecovered)
@@ -38,7 +40,8 @@ function showCounts(fmatCase, fmatDeath, fmatRecovered){
     cases.textContent = `${fmatCase}`;
     recoverd.textContent = `${fmatRecovered}`;
     death.textContent = `${fmatDeath}`;
-    
+    showcard(fmatCase)
+
 }
 
 function showMaps(){
@@ -47,7 +50,6 @@ function showMaps(){
 
 
 async function getCountries(){
-    let COUNTRY_API = "https://restcountries.eu/rest/v2/";
     let result = await fetch(COUNTRY_API);
     let data = await result.json();
     let dropdown = document.querySelector('.countries');
@@ -57,7 +59,6 @@ async function getCountries(){
     
     data.map((data)=>{
         let name = data['name']
-        let flag = data['flag']
         let option = document.createElement('option');
         option.value = name;
         option.innerHTML = name;
@@ -69,43 +70,50 @@ getCountries()
 
 
 async function showcard(){
-    let COUNTRY_API = "https://restcountries.eu/rest/v2/";
     let affectedcont = document.querySelector(".affected-cont");
-    let result = await fetch(COUNTRY_API);
+    let result = await fetch(CASE_API_Country);
     let data = await result.json();
-    let oup;
     data.map((data)=>{
-        let name = data['name']
-        let flag = data['flag']
+        let name = data['country']
+        let cases = data['cases']
         let cardCont = document.createElement("div")
         cardCont.setAttribute("class", "card-cont")
         cardCont.innerHTML = `
             <div class="clear-fix">
                 <div class="float-left">
-                    <img src="${flag}" alt="flag">
+                    <img alt="flag" class="flag-img">
                     <small class="ml-1">${name}</small>
                 </div>
 
                 <div class="float-right">
-                    <small class="mr-1">1123.5</small>
+                    <small class="mr-1 country-case-val">${cases}</small>
                 </div>
             </div>
         `;
         affectedcont.appendChild(cardCont)
     })
+    
+    appendCountryFlag();
 }
-showcard()
-// function populateDropdwn(name){
-//     let dropdown = document.querySelector('.countries');
-//     let form = document.mquerySelector(".countries-form-cont");
-//     dropdown.innerHTML = `
-//         <select class="form-control countries">
-//         <option value="">Select Country</option>
-//         <option value="">Global</option>
-//         <option value="${name}">${name}</option>
-//         </select>
-//     `;
-// }
+
+
+async function appendCountryFlag(){
+
+    let result = await fetch(COUNTRY_API);
+    let data = await result.json();
+    let flag = document.querySelectorAll(".flag-img")
+    let flagsrc = data.map((data)=>{
+        let src = data.flag;
+        return src
+    })
+    console.log();
+    flag.forEach((img)=>{
+        // img.src = flagsrc;
+        console.log(data.flag);
+    })
+}
+
+
 // api
 // https://api.kawalcorona.com/
 // countries api : https://restcountries.eu/rest/v2/name/nigeria
